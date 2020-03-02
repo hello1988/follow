a951b182639124c5541fff4e08133641496b0207
hello1988 <hello19881990@hotmail.com>
2020-03-02 17:08:54 +0800

    切換工作目錄

--- rename.py
+++ rename.py
@@ -24,6 +24,8 @@ sub_version = argv[0] if len(argv) > 0 else input('請輸入版控類型:\n')
 sub_version = SVN if sub_version.lower() == 'svn' else Git
 
 base_file = argv[1] if len(argv) > 1 else input('請輸入檔案路徑:\n')
+os.chdir(base_file)
+
 replacer = argv[2] if len(argv) > 2 else input('請輸入欲修改的檔名規則(正則):\n')
 target = argv[3] if len(argv) > 3 else input('請輸入欲取代的字串):\n')
 
